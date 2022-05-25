1bfcd31e6980d78731cbebe8b402c36c92554c39
cole <cole@opteqint.net>
2022-05-25 21:38:30 +0200

    Added references

--- WinUI3PythonIncluded/MainWindow.xaml.cs
+++ WinUI3PythonIncluded/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using Python.Included;
-using Python.Runtime;
-
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -16,6 +13,10 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using System.Threading.Tasks;
+
+using Python.Included;
+using Python.Runtime;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -35,24 +36,70 @@ namespace WinUI3PythonIncluded
         private void myButton_Click( object sender, RoutedEventArgs e )
         {
             myButton.Content = "Clicked";
-            Task.Run( ( ) => this.SetupPython( ) );
+
+            /*
+            string original_path = System.Environment.GetEnvironmentVariable( "PATH" );
+            System.Diagnostics.Debug.WriteLine( "\n\nPATH: {0}", original_path, null );
+
+            string python_path = Installer.EmbeddedPythonHome;
+            string new_path = original_path + ";" + python_path;
+
+            System.Diagnostics.Debug.WriteLine( "New_path: {0}", new_path, null );
+            System.Environment.SetEnvironmentVariable( "PATH", new_path );
+            */
+
+            
+            Installer.InstallPath = @"C:\Testing\";
+
+            System.Diagnostics.Debug.WriteLine( "\n\n\nPythonDLL: {0}", Runtime.PythonDLL, null );
+            System.Diagnostics.Debug.WriteLine( "PythonHome: {0}", Installer.EmbeddedPythonHome, null );
+
+            string python_dll_path = Path.Combine( Installer.EmbeddedPythonHome, "python310.dll" );
+            System.Diagnostics.Debug.WriteLine( "Path: {0}", python_dll_path, null );
+
+
+            var task = Installer.SetupPython( );
+            Task.WhenAll( task );
+            System.Diagnostics.Debug.WriteLine( "\n\n\nSetup finished\n\n\n" );
+
+            Runtime.PythonDLL = python_dll_path;
+
+            PythonEngine.Initialize( );
+            using ( Py.GIL( ) )
+            {
+                dynamic sys = Py.Import( "sys" );
+                string version = sys.version;
+                System.Diagnostics.Debug.WriteLine( "VERSIOn: {0}", version, null );
+            }
+
+
+            //Task.Run( ( ) => this.SetupPython( ) );
         }
+
+
 
         private async Task SetupPython( )
         {
             string path = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
             System.Diagnostics.Debug.WriteLine( "\n\n\nPath: {0}\n\n\n", path, null );
 
+            string python_path = Path.Combine( path, "python-3.7.3-embed-amd64", "python37.dll" );
 
-            Installer.InstallPath = path;
+            System.Diagnostics.Debug.WriteLine( "Pythonpath: {0}", python_path, null );
+
+            System.Diagnostics.Debug.WriteLine( "InstallPath: {0}", Installer.InstallPath, null );
+            System.Diagnostics.Debug.WriteLine( "EmbeddedPythonHome: {0}", Installer.EmbeddedPythonHome, null );
+
+            //Installer.InstallPath = path;
             await Installer.SetupPython( force: true );
             PythonEngine.Initialize( );
-            PythonEngine.BeginAllowThreads( );
+
+            //PythonEngine.BeginAllowThreads( );
 
             using ( Py.GIL( ) )
             {
                 dynamic sys = Py.Import( "sys" );
-                string version = $"{ sys.version }";
+                string version = sys.version;
                 System.Diagnostics.Debug.WriteLine( "version: {0}", version, null );
             }
         }
